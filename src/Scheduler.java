2da3e0266b15dc23d5851b1256d3d4b594551f7e
G1TS23 <61652480+G1TS23@users.noreply.github.com>
2026-01-13 10:12:54 +0100

    Test init (#1)
    * :rotating_light: test d'initialisation
    
    * :white_check_mark:  test d'initialisation
    
    * :recycle: test d'initialisation
    
    * :rotating_light: test d'initialisation avec argument null
    
    * :white_check_mark: test d'initialisation avec argument null
    
    * :recycle: test d'initialisation avec argument null

--- src/Scheduler.java
+++ src/Scheduler.java
@@ -1,2 +1,13 @@
+import java.time.Clock;
+import java.util.Objects;
+
 public class Scheduler {
+    private final Clock clock;
+
+    public Scheduler(Clock clock) {
+        if (Objects.isNull(clock)) {
+            throw new IllegalArgumentException("clock cannot be null");
+        }
+        this.clock = clock;
+    }
 }
--- test/SchedulerTest.java
+++ test/SchedulerTest.java
@@ -1,2 +1,20 @@
+import org.junit.jupiter.api.Test;
+
+import java.time.Clock;
+import java.time.ZoneId;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class SchedulerTest {
+
+    @Test
+    void doitInitialiserScheduler() {
+        Scheduler scheduler = new Scheduler(Clock.system(ZoneId.of("Europe/Paris")));
+        assertNotNull(scheduler);
+    }
+
+    @Test
+    void neDoitPasInitialiserScheduler() {
+        assertThrows(IllegalArgumentException.class, () -> new Scheduler(null));
+    }
 }
