e1891c7dff73b649def678a365689bd86e1a2ea6
olivierfalahi <olivier.falahi@gmail.com>
2026-01-13 16:55:23 +0100

    :rotating_light: test d'ajout d'une tâche

--- test/SchedulerTest.java
+++ test/SchedulerTest.java
@@ -2,6 +2,7 @@ import org.junit.jupiter.api.Test;
 
 import java.time.Clock;
 import java.time.ZoneId;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -26,5 +27,19 @@ public class SchedulerTest {
         Scheduler scheduler = new Scheduler(Clock.system(ZoneId.of("Europe/Paris")));
         assertNotNull(scheduler.getTasks());
         assertTrue(scheduler.getTasks().isEmpty());
+    }
+
+    @Test
+    void doitAjouterUneTache(){
+        Scheduler scheduler = new Scheduler(Clock.system(ZoneId.of("Europe/Paris")));
+        Scheduler.Task task = new Scheduler.Task("backup", "* * 12 1/1 * ? *", () -> {});
+
+        scheduler.setTask("backup", "* * 12 1/1 * ? *", () -> {System.out.println("backup");});
+        List<Scheduler.Task> tasks = scheduler.getTasks();
+
+        assertEquals(1, tasks.size());
+        assertEquals(task.name, tasks.getFirst().name);
+        assertEquals(task.periodicity, tasks.getFirst().periodicity);
+        assertDoesNotThrow(() ->tasks.getFirst().runnable.run());
     }
 }
