6d89aa0db2decade4c6a0bca0d73fdb64c6251f2
olivierfalahi <olivier.falahi@gmail.com>
2026-01-13 09:53:40 +0100

    :white_check_mark: test d'initialisation avec argument null

--- src/Scheduler.java
+++ src/Scheduler.java
@@ -4,6 +4,9 @@ public class Scheduler {
     private final Clock clock;
 
     public Scheduler(Clock clock) {
+        if (clock == null) {
+            throw new IllegalArgumentException("clock cannot be null");
+        }
         this.clock = clock;
     }
 }
