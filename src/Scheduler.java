3ff77bdc6bbe7edcf8dfdab6ba3b4ddb4beeb2d9
olivierfalahi <olivier.falahi@gmail.com>
2026-01-13 10:08:45 +0100

    :recycle: test d'initialisation avec argument null

--- src/Scheduler.java
+++ src/Scheduler.java
@@ -1,10 +1,11 @@
 import java.time.Clock;
+import java.util.Objects;
 
 public class Scheduler {
     private final Clock clock;
 
     public Scheduler(Clock clock) {
-        if (clock == null) {
+        if (Objects.isNull(clock)) {
             throw new IllegalArgumentException("clock cannot be null");
         }
         this.clock = clock;
